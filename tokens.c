56b603419e04a7fd28ff18d28d1974770d93da86
lepoi <terminallyquack@gmail.com>
2018-12-04 23:28:22 -0800

    added K_PRLN to token_names string

--- tokens.c
+++ tokens.c
@@ -1,4 +1,4 @@
-char *token_names = "O_SUM   O_INC   O_SUB   O_DEC   O_MUL   O_DIV   O_MOD   O_NOT   O_NEQ   O_LES   O_LEEQ  O_GRE   O_GREQ  S_ASGN  O_EQ    O_AND   O_OR    K_CHR   K_INT   K_FLT   K_STR   K_IF    K_EIF   K_ELSE  K_WHLE  K_FOR   K_PRNT  K_READ  K_BRK   K_CONT  S_LPAR  S_RPAR  S_LCBR  S_RCBR  S_LSBR  S_RSBR  S_SCLN  S_PNT   S_CMA   T_IDEN  T_CHR   T_INT   T_FLT   T_STR   T_EOF";
+char *token_names = "O_SUM   O_INC   O_SUB   O_DEC   O_MUL   O_DIV   O_MOD   O_NOT   O_NEQ   O_LES   O_LEEQ  O_GRE   O_GREQ  S_ASGN  O_EQ    O_AND   O_OR    K_CHR   K_INT   K_FLT   K_STR   K_IF    K_EIF   K_ELSE  K_WHLE  K_FOR   K_PRNT  K_PRLN  K_READ  K_BRK   K_CONT  S_LPAR  S_RPAR  S_LCBR  S_RCBR  S_LSBR  S_RSBR  S_SCLN  S_PNT   S_CMA   T_IDEN  T_CHR   T_INT   T_FLT   T_STR   T_EOF";
 
 enum token_type {
 	O_SUM,	O_INC,	O_SUB,	O_DEC,	O_MUL,	O_DIV,	O_MOD,	O_NOT,	O_NEQ,
